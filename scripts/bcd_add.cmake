7f1b9fe06f2582ded8a0ba800f516127898a1246
Hussein Itawi <hus@Husseins-MacBook-Pro.local>
2022-08-24 12:32:15 +0300

    Add extra arguments

--- scripts/bcd_add.cmake
+++ scripts/bcd_add.cmake
@@ -11,6 +11,7 @@ function (bcd_add target isInterface isGitRepo gitTag url)
                 -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                 -DCMAKE_INSTALL_PREFIX:PATH=${EXTERNAL_LIBS_DIR}/${target}
                 -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
+                ${ARGN}
         )
     endif()
 
